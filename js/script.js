ab24f3b14f12ddf7e1e5e429ef9a84f99b16acea
James Gordon <gordo@mwc-225231.dhcp.missouri.edu>
2015-01-27 19:55:07 -0600

    simple ajax call to load some data

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
+var playerData;
+
 $(document).ready(function() {
-    console.log("Hello world.")
+    console.log("Hello world.");
+    getData();
 });
 
+function getData() {
+	$.getJSON('js/baseballcard.json', function(data) {
+		playerData = data;
+		drawStuff();
+	})
+};
 
+function drawStuff() {
+	$(".chart").html(playerData.name);
 
+	$.each(playerData.stats, function(i, item)
+		{
+			$(".chart").append("<p>" + item.AVG + '</p>');
+		})
+};
 
 
 
